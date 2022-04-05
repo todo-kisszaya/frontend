3bb16c98ee76d2feac49f4638c3a999b1bb109c5
kisszaya <sveta.matvik.sveta@gmail.com>
2022-04-05 15:44:11 +0300

    feat(entities/tasks/ui/tasks-list): tsx

--- src/entities/tasks/ui/tasks-list/tasks-list.tsx
+++ src/entities/tasks/ui/tasks-list/tasks-list.tsx
@@ -1,5 +1,8 @@
 import {GetAllTasksResponse} from "entities/tasks/api/types";
-import { Grid } from "shared/ui";
+import {Grid, LinkCard} from "shared/ui";
+import {generatePath} from "react-router";
+import {PRIVATE_PATH} from "shared/config";
+import {Empty, Typography} from "antd";
 
 interface Props {
     tasks: GetAllTasksResponse | undefined
@@ -7,6 +10,19 @@ interface Props {
 
 export const TasksList = ({tasks}: Props) => {
     return <Grid>
-        meow
+        {
+            !tasks || !!tasks.tasks.length ? (
+                tasks?.tasks.map(task => (
+                    <LinkCard key={task.task_id}
+                              href={generatePath(PRIVATE_PATH.TASK, {taskId: task.task_id.toString()})}>
+                        <Typography.Title level={4}>
+                            {task.name}
+                        </Typography.Title>
+                    </LinkCard>
+                ))
+            ) : (
+                <Empty description="Пока что нет тасков"/>
+            )
+        }
     </Grid>
 }
